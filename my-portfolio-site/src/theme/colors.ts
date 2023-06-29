64d1b7b29cfda7b0988b151ff1a24ad5cba2c276
Joey Quandt <57623466+JoeyQuandt@users.noreply.github.com>
2023-06-29 10:58:10 +0200

    feat(generic styling): added generic styling

---
+++ my-portfolio-site/src/theme/colors.ts
@@ -0,0 +1,12 @@
+import { ChakraTheme } from '@chakra-ui/react';
+
+export const colors: ChakraTheme['colors'] = {
+  blockchain: '#C4FF3B',
+  multiPlatform: '#FF2BD1',
+  AI: '#B03BFF',
+  AR: '#5DF2F5',
+  VR: '#FF8A00',
+  Experiental: '#32009E',
+  grey: '#DDDDDD',
+  darkGrey: '#A6A6A6',
+};
---
+++ my-portfolio-site/src/theme/typography.ts
@@ -0,0 +1,6 @@
+import { ChakraTheme } from '@chakra-ui/react';
+
+export const fonts: ChakraTheme['fonts'] = {
+  heading: `Arcade Gamer, sans-serif`,
+  body: `Maison Neue, sans-serif`,
+};
