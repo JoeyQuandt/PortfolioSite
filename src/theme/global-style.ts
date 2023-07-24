661bbf155728c22883aa9be2df8aac4946f1b66b
Joey Quandt <57623466+JoeyQuandt@users.noreply.github.com>
2023-07-24 15:46:15 +0200

    feat(chakra styling setup): chakra styling setup

---
+++ src/theme/global-style.ts
@@ -0,0 +1,13 @@
+import { SystemStyleObject } from '@chakra-ui/react';
+
+export const globalStyle: SystemStyleObject = {
+  'html, body': {
+    backgroundColor: 'white',
+  },
+  body: {
+    minWidth: '320px',
+  },
+  '*': {
+    boxSizing: 'border-box',
+  },
+};
