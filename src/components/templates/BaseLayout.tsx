0b8f108eb3027e0ccb191c9bb1f6e6eaeffb5f66
Joey Quandt <57623466+JoeyQuandt@users.noreply.github.com>
2023-07-31 15:28:17 +0200

    feat(added backgroundprops): added backgroundprops

--- src/components/templates/BaseLayout.tsx
+++ src/components/templates/BaseLayout.tsx
@@ -4,6 +4,9 @@ import { ReactNode } from 'react';
 import { Footer } from '../features/layout/Footer';
 import { Header } from '../features/layout/Header';
 
+import { OvalIcon,CircleIcon } from '../shared/backgroundIcons';
+
+
 
 
 interface BaseLayoutProps {
@@ -12,12 +15,14 @@ interface BaseLayoutProps {
 
 export function BaseLayout({ children }: BaseLayoutProps) {
   return (
-    <Flex flexDirection="column" height="100vh" paddingInline={[4,8,8,44]}>
+    <Flex flexDirection="column" height="100vh" paddingInline={[4,8,8,44]} position="relative">
       <Header />
       <Box as="main" flex="1 0 auto" display="block">
         <Container>{children}</Container>
       </Box>
       <Footer />
+      <OvalIcon position="absolute" top="0" left="0"/>
+      <CircleIcon position="absolute" bottom="0" right="0"/>
     </Flex>
   );
 }
