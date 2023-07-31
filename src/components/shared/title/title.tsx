96f7464741c50e8986b5d1cb766561948a2ceac9
Joey Quandt <57623466+JoeyQuandt@users.noreply.github.com>
2023-07-31 15:59:21 +0200

    feat(skills): skills component

---
+++ src/components/shared/title/title.tsx
@@ -0,0 +1,20 @@
+import { Heading,Text,Flex } from "@chakra-ui/react"
+
+type TitleProps = {
+    title: string,
+    text: string,
+}
+
+
+export function Title({title,text} : TitleProps){
+    return(
+        <Flex color="white" flexDirection="column" textAlign={["center","left"]} gap={3.5}>
+            <Heading as="h2" textTransform={title==="html" || title==="css" ? "uppercase" : "capitalize"}>
+                {title}
+            </Heading>
+            <Text>
+                {text}
+            </Text>
+        </Flex>
+    )
+}
