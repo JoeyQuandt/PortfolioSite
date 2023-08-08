dd7cda535698d7260c7277ec6ee786f61cc3c95e
Joey Quandt <57623466+JoeyQuandt@users.noreply.github.com>
2023-08-08 12:00:03 +0200

    feat(zIndex): added zIndex

--- src/components/shared/backgroundIcons/BackgroundIcons.tsx
+++ src/components/shared/backgroundIcons/BackgroundIcons.tsx
@@ -1,9 +1,25 @@
 import { Image } from '@chakra-ui/react';
 
 export function OvalIcon({ ...props }) {
-  return <Image src="./oval.svg" alt="background-icon" width={{ lg: '300px' }} {...props} />;
+  return (
+    <Image
+      src="./oval.svg"
+      alt="background-icon"
+      width={{ lg: '300px' }}
+      zIndex={{ lg: 3 }}
+      {...props}
+    />
+  );
 }
 
 export function CircleIcon({ ...props }) {
-  return <Image src="./circle.svg" alt="background-icon" width={{ lg: '100px' }} {...props} />;
+  return (
+    <Image
+      src="./circle.svg"
+      alt="background-icon"
+      width={{ lg: '100px' }}
+      zIndex={{ lg: 3 }}
+      {...props}
+    />
+  );
 }
