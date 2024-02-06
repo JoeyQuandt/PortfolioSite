2397fad094b67f064308dfdbe64975af8bbaef80
Joey Quandt <57623466+JoeyQuandt@users.noreply.github.com>
2024-02-06 18:44:48 +0100

    feat(added prismic preview)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,7 @@
 import { renderFavicons } from '@/utils';
 import { Html, Head, Main, NextScript } from 'next/document';
+import { repositoryName } from '@/prismicio';
+import { PrismicPreview } from '@prismicio/next';
 
 export default function Document() {
   return (
@@ -10,6 +12,7 @@ export default function Document() {
       </Head>
       <body>
         <Main />
+        <PrismicPreview repositoryName={repositoryName} />
         <NextScript />
       </body>
     </Html>
