abb6b9d6917290922907f47bc068f74948a14229
Joey Quandt <57623466+JoeyQuandt@users.noreply.github.com>
2024-02-14 20:31:07 +0100

    feat(sentry setup)

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -1,4 +1,5 @@
 import { NextPage, NextPageContext } from 'next';
+import * as Sentry from '@sentry/nextjs';
 
 import { ErrorPage } from '@/components/templates';
 
@@ -9,6 +10,7 @@ export interface ErrorProps {
 const MyError: NextPage<ErrorProps> = props => <ErrorPage {...props} />;
 
 MyError.getInitialProps = async ({ res, err }: NextPageContext) => {
+  await Sentry.captureUnderscoreErrorException({ res });
   const statusCode = res ? res.statusCode : err ? err.statusCode : null;
   return { statusCode };
 };
