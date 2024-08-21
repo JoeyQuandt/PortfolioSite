79304fc61425f1875678065e83cfd9c9d513b5c6
Joey Quandt <57623466+JoeyQuandt@users.noreply.github.com>
2024-08-21 15:58:30 +0200

    feat(setup tina, react)

---
+++ astro.config.mjs
@@ -0,0 +1,27 @@
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
+
+const tina = ({ directiveName = "tina" } = {}) => ({
+  name: "tina-cms",
+  hooks: {
+    "astro:config:setup": ({ addClientDirective, opts }) => {
+      addClientDirective({
+        name: directiveName,
+        entrypoint: "./client-directives/tina.mjs",
+      });
+    },
+  },
+});
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    tina(),
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+});
