ca7325a9e2a212e7ace6e91d217b16dbb6608d57
Joey Quandt <57623466+JoeyQuandt@users.noreply.github.com>
2024-08-29 21:28:03 +0200

    fix(css)

--- src/components/TinaComponents/Skills.tsx
+++ src/components/TinaComponents/Skills.tsx
@@ -9,25 +9,27 @@ type SkillsProps = {
 
 export default function Skills({ data }: SkillsProps) {
   return (
-    <section className="layout pb-20 lg:pb-44 relative">
-      <Oval className="rotate-180 right-[-250px] bottom-28 lg:bottom-20 absolute" />
-      <Separator className="mb-16" />
-      <ul
-        data-tina-field={tinaField(data, "skill")}
-        className="grid max-md:place-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-[100px] max-md:mb-16"
-      >
-        {data?.skill?.map((item, index) => {
-          return (
-            <li
-              className="text-5xl leading-[56px] text-white font-bold"
-              key={index}
-            >
-              {item}
-            </li>
-          );
-        })}
-      </ul>
-      <Separator className="max-md:block hidden" />
+    <section className=" relative">
+      <Oval className="rotate-180 right-[-250px]  bottom-28 lg:bottom-20 absolute" />
+      <div className="layout pb-20 lg:pb-44">
+        <Separator className="mb-16" />
+        <ul
+          data-tina-field={tinaField(data, "skill")}
+          className="grid max-md:place-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-[100px] max-md:mb-16"
+        >
+          {data?.skill?.map((item, index) => {
+            return (
+              <li
+                className="text-5xl leading-[56px] text-white font-bold"
+                key={index}
+              >
+                {item}
+              </li>
+            );
+          })}
+        </ul>
+        <Separator className="max-md:block hidden" />
+      </div>
     </section>
   );
 }
