d91dc7af2c5a9961f21ece186e0d15adcf6797fd
Joey Quandt <57623466+JoeyQuandt@users.noreply.github.com>
2024-08-29 20:30:57 +0200

    fix(removed old code)

--- src/utils/findObjectInArray.ts
+++
@@ -1,8 +0,0 @@
-import { PageContentpageContentHomeUnion } from '@/gql/graphql';
-
-export const findObjectInArray = <T extends PageContentpageContentHomeUnion>(
-  arr: PageContentpageContentHomeUnion[],
-  name: string,
-): T | undefined => {
-  return arr.find(item => item?.__typename === name) as T | undefined;
-};
